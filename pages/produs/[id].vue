170d5f950beac7369f76a622e5b152438e290dfd
xeemax <xeemax2.0@gmail.com>
2024-10-22 23:22:25 +0500

    - displayed products or products page - added slugs - added optional chaining

---
+++ pages/produs/[id].vue
@@ -0,0 +1,54 @@
+<script setup>
+import {useRoute} from 'vue-router';
+import {ref} from 'vue';
+import {checkQuieries} from '~/composables/useFilter';
+import axios from "axios";
+// import { useFilter } from '~/composables/useFilter';
+
+const product = ref({});
+const variantProduct = ref({});
+
+const {params, query: q} = useRoute();
+const query = ref(q);
+const productId = params.id.split('_')[1];
+const key = ref(0)
+
+const getProduct = async () => {
+    const res = await axios.get(`/api/product?id=${productId}`);
+    product.value = res.data
+    variantProduct.value = checkQuieries(product.value.variants, query.value);
+    key.value += 1
+}
+onMounted(() => {
+    getProduct()
+})
+
+</script>
+
+<template>
+    <h1>Produs Id: {{ productId }}</h1>
+
+  <!-- BIG MAIN -->
+    <div class="flex">
+        <!-- Product Primary abd Secondary Images -->
+        <ProductImage
+                :key="key"
+                :product="product.product"
+                :product-variant="variantProduct"
+        />
+
+        <!-- Product Desc -->
+        <ProductDescription
+                :key="key"
+                :product="product.product"
+                :product-variant="variantProduct"
+        />
+
+        <!-- Delivery -->
+    </div>
+  <!-- Select Variants -->
+  <!--  <ProductVariants-->
+  <!--    :variants="product.variants"-->
+  <!--    :variant-product="variantProduct"-->
+  <!--  />-->
+</template>
