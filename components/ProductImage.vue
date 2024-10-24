439707cc0351fd9e5d628a61fb0bbb02655b9229
cristiaprodan <prodancristianbusiness@gmail.com>
2024-10-24 11:39:02 +0300

    fixed productImage

--- components/ProductImage.vue
+++ components/ProductImage.vue
@@ -1,48 +1,164 @@
-<script setup>
-import {ref} from 'vue';
+<script setup lang="ts">
+import { ref, watch } from "vue";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  Carousel,
+  type CarouselApi,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import { watchOnce } from "@vueuse/core";
 
 const props = defineProps({
-    product: {type: Object, required: true},
-    productVariant: {required: true},
+  product: { type: Object, required: true },
+  productVariant: { required: true },
 });
-const {product, productVariant} = props;
+const { product, productVariant } = props;
 
-const image = ref(product?.Imagine_Principala[0]);
+const emblaMainApi = ref<CarouselApi>();
+const emblaThumbnailApi = ref<CarouselApi>();
+const selectedIndex = ref(0);
+
+const image = ref(product?.Imagine_Principala?.[0] || "");
+
+function onSelect() {
+  if (!emblaMainApi.value || !emblaThumbnailApi.value) return;
+  selectedIndex.value = emblaMainApi.value.selectedScrollSnap();
+  emblaThumbnailApi.value.scrollTo(emblaMainApi.value.selectedScrollSnap());
+}
+
+function onThumbClick(index: number) {
+  if (!emblaMainApi.value || !emblaThumbnailApi.value) return;
+  emblaMainApi.value.scrollTo(index);
+}
+
+watchOnce(emblaMainApi, (emblaMainApi) => {
+  if (!emblaMainApi) return;
+
+  onSelect();
+  emblaMainApi.on("select", onSelect);
+  emblaMainApi.on("reInit", onSelect);
+});
 
 watch(
-    () => productVariant,
-    () => {
-        console.log(productVariant);
-        if (productVariant.value) {
-            if (productVariant.value.Imagini) {
-                image.value = productVariant.value.Imagini[0];
-                return;
-            }
-        }
-        if (productVariant.Imagini) {
-            image.value = productVariant.value.Imagini[0];
-            return;
-        }
-        // variantName.value = getProductName();
-    },
-    {deep: true, immediate: true}
+  () => productVariant,
+  (newProductVariant) => {
+    if (newProductVariant && typeof newProductVariant === "object") {
+      if (
+        "Imagini" in newProductVariant &&
+        Array.isArray(newProductVariant.Imagini)
+      ) {
+        image.value = newProductVariant.Imagini[0] || "";
+        return;
+      }
+    }
+    image.value = product?.Imagine_Principala?.[0] || "";
+  },
+  { deep: true, immediate: true }
 );
 </script>
 
 <template>
-    <div class="w-fit flex flex-col gap-4">
-        <UCard class="w-fit">
-            <img :src="image" alt="" class="object-contain min-w-80" loading="lazy"/>
-        </UCard>
-        <UCarousel
-                v-slot="{ item }"
-                :items="product?.imagini_Secundare"
-                :ui="{ item: 'basis-1/4' }"
-                class="flex gap-2"
+  <div class="w-full max-w-3xl mx-auto">
+    <!-- Main Carousel -->
+    <Carousel
+      class="w-full sm:max-w-xl mx-auto mb-4"
+      @init-api="(val) => (emblaMainApi = val)"
+    >
+      <CarouselContent>
+        <CarouselItem>
+          <Card
+            class="bg-white dark:bg-charade-950 border-gray-200 dark:border-charade-700"
+          >
+            <CardContent
+              class="flex aspect-square items-center justify-center p-0"
+            >
+              <img
+                :src="image"
+                alt="Main Product Image"
+                class="h-full w-full object-cover rounded-lg"
+              />
+            </CardContent>
+          </Card>
+        </CarouselItem>
+        <CarouselItem
+          v-for="(item, index) in product?.imagini_Secundare"
+          :key="index"
         >
-            <div class="p-2 border-2 border-gray-500 m-2 rounded-lg" @click="image = item">
-                <img :src="item" alt="" class="m-1 aspect-square object-cover"/>
-            </div>
-        </UCarousel>
-    </div>
+          <Card
+            class="bg-white dark:bg-charade-950 border-gray-200 dark:border-charade-700"
+          >
+            <CardContent
+              class="flex aspect-square items-center justify-center p-0"
+            >
+              <img
+                :src="item"
+                alt=""
+                class="h-full w-full object-cover rounded-lg"
+              />
+            </CardContent>
+          </Card>
+        </CarouselItem>
+      </CarouselContent>
+      <CarouselPrevious class="hidden sm:flex" />
+      <CarouselNext class="hidden sm:flex" />
+    </Carousel>
+
+    <!-- Thumbnail Carousel -->
+    <Carousel
+      class="w-full sm:max-w-xl mx-auto"
+      @init-api="(val) => (emblaThumbnailApi = val)"
+    >
+      <CarouselContent class="flex gap-2">
+        <CarouselItem
+          class="basis-1/4 min-w-0 cursor-pointer pl-0"
+          @click="onThumbClick(0)"
+        >
+          <div :class="[0 === selectedIndex ? 'opacity-100' : 'opacity-50']">
+            <Card
+              class="bg-white dark:bg-charade-950 border-gray-200 dark:border-charade-700"
+            >
+              <CardContent
+                class="flex aspect-square items-center justify-center p-2"
+              >
+                <img
+                  :src="image"
+                  alt="Main Product Image Thumbnail"
+                  class="h-full w-full object-cover rounded-lg"
+                />
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+        <CarouselItem
+          v-for="(item, index) in product?.imagini_Secundare"
+          :key="index"
+          class="basis-1/4 min-w-0 cursor-pointer pl-0"
+          @click="onThumbClick(index + 1)"
+        >
+          <div
+            :class="[
+              index + 1 === selectedIndex ? 'opacity-100' : 'opacity-50',
+            ]"
+          >
+            <Card
+              class="bg-white dark:bg-charade-950 border-gray-200 dark:border-charade-700"
+            >
+              <CardContent
+                class="flex aspect-square items-center justify-center p-2"
+              >
+                <img
+                  :src="item"
+                  alt=""
+                  class="h-full w-full object-cover rounded-lg"
+                />
+              </CardContent>
+            </Card>
+          </div>
+        </CarouselItem>
+      </CarouselContent>
+    </Carousel>
+  </div>
 </template>
